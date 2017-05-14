74bf6425b6a07a3658e77b98b3cdbb6b04532511
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-05-14 14:10:19 +0700

    Migrate mockup from athens repo & add vue-router to npm

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+<body>
+    <div>{{ msg }}</div>
+</body>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello vue"
+        }
+    }
+};
+</script>
+
+<style>
+.message {
+    color: blue;
+}
+</style>
