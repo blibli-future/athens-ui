27c9cf1bc02253911545d11ba87204edcc591c80
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-05-14 13:46:28 +0700

    Initial commit for vuejs config
    Custom minimalist file based on vue-cli webpack-simple template

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname,
+        filename: 'athens.js'
+    },
+    module : {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loader: {
+                        js: 'babel-loader'
+                    }
+                }
+            }
+
+        ]
+
+    }
+}
