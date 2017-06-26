a2bff6279f38aea9d8c48c9e3dca75df3ab5e714
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-06-26 12:34:36 +0700

    re-css apply, history, and summary

--- src/component/request/Apply.vue
+++ src/component/request/Apply.vue
@@ -1,97 +1,117 @@
 <template>
-    <div>
-        <h3>Apply for absence request</h3>
-        <form>
-            <div class="col--md-3">
-                <label for="criteria">Criteria</label>
-                <select id="criteria" class="form--item">
-                    <option>Cuti istimewa</option>
-                    <option>Cuti tahunan</option>
-                    <option>Ijin sakit</option>
-                    <option>Sakit dengan surat dokter</option>
-                </select>
+    <section class="apply">
+        <h3 class="apply__title">Apply for absence request</h3>
+        <form class="apply__wrapper">
+            <div class="apply__wrapper--criteria">
+                <div class="apply__wrapper--main-criteria">
+                    <label class="apply__label" for="criteria">Criteria</label>
+                    <select class="apply__selector" id="criteria">
+                        <option>Cuti istimewa</option>
+                        <option>Cuti tahunan</option>
+                        <option>Ijin sakit</option>
+                        <option>Sakit dengan surat dokter</option>
+                    </select>
+                </div>
+                <div class="apply__wrapper--sub-criteria">
+                    <label class="apply__label" for="sub-criteria">Sub Criteria</label>
+                    <select class="apply__selector" id="sub-criteria">
+                        <option>Cuti istimewa</option>
+                        <option>Cuti tahunan</option>
+                        <option>Ijin sakit</option>
+                        <option>Sakit dengan surat dokter</option>
+                    </select>
+                </div>
             </div>
-            <div class="col--md-3">
-                <label for="sub-criteria">Sub Criteria</label>
-                <select id="sub-criteria" class="form--item">
-                    <option>Cuti istimewa</option>
-                    <option>Cuti tahunan</option>
-                    <option>Ijin sakit</option>
-                    <option>Sakit dengan surat dokter</option>
-                </select>
+            <div class="apply__wrapper--date">
+                <div class="apply__wrapper--start-date">
+                    <label class="apply__label" for="start-date">Start Date</label>
+                    <input id="start-date" type="date"/>
+                </div>
+                <div class="apply__wrapper--end-date">
+                    <label class="apply__label" for="end-date">End Date</label>
+                    <input id="end-date" type="date"/>
+                </div>
             </div>
-            <div class="col--md-3">
-                <label for="start-date">Start Date</label>
-                <input id="start-date" type="date" class="form--item"/>
+            <div class="apply__wrapper apply__wrapper--reason">
+                <label class="apply__label--reason" for="reason">Reason :</label>
+                <textarea id="reason" class="apply__reason--input"></textarea>
             </div>
-            <div class="col--md-3">
-                <label for="end-date">End Date</label>
-                <input id="end-date" type="date"  class="form--item" />
-            </div>
-            <div class="col--md-12">
-                <label for="reason">Reason :</label>
-                <textarea id="reason" class="form--item" rows="6"></textarea>
-            </div>
-            <button class="button">Apply</button>
+            <button class="apply__submit">Apply</button>
         </form>
-    </div>
+    </section>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
-  }
 }
 </script>
 
-<style>
-    textarea .form--item {
-        height: auto;
-    }
-    .form--item{
-        display: block;
-        width: 100%;
-        height: 30px;
-        padding: 5px 10px;
-        font-size: 14px;
-        line-height: 1.5;
-        color: #555;
-        background-color: #fff;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-        box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075)
+<style lang="scss">
+    .apply {
+        &__title {
+            color: rgb(51, 51, 51);
+            font-size: 20px;
+            font-weight: 500;
+            line-height: 26.4px;
+            margin-bottom: 10px;
+        }
 
-    }
-    .col--md-3{
-        width:24%;
-        float: left;
-        margin: 5px;
-    }
-    .col--md-12{
-    width:99%;
-    float: left;
-    margin:5px;
-    }
-    .button{
-        margin-left: 5px;
-        display: inline-block;
-        padding: 8px 12px;
-        margin-bottom: 0;
-        font-size: 14px;
-        text-align: center;
-        border: 1px solid transparent;
-        border-radius: 4px;
-        background-color: #b3dcff;
-        cursor: pointer;
-    }
-    .button:hover{
-        background-color: #92ccff;
-    }
-    input[type="date"]{
-        font-family: Arial;
-        overflow: hidden;
-        padding: 5px;
+        &__wrapper {
+            display: block;
+            margin: 1rem 0;
+
+            &--criteria, &--date {
+                display: inline-block;
+                width: 48%;
+            }
+
+            &--main-criteria, &--sub-criteria {
+                margin: 0.5rem 0;
+                display: block;
+            }
+
+            &--start-date, &--end-date {
+                margin: 0.5rem 0;
+                display: block;
+            }
+        }
+
+        &__label {
+            width: 100px;
+            display: inline-block;
+
+            &--reason {
+                display: block;
+                margin: 0 0 5px 0;
+            }
+        }
+
+        &__selector {
+            display: inline-block;
+            height: 30px;
+            padding: 5px 10px;
+            font-size: 14px;
+            line-height: 1.5;
+            color: #555;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075)
+        }
+
+        &__submit {
+            padding: 8px 12px;
+            font-size: 14px;
+            text-align: center;
+            border: 1px solid transparent;
+            border-radius: 4px;
+            background-color: #b3dcff;
+            cursor: pointer;
+        }
+
+        &__reason--input {
+            width: 100%;
+        }
     }
 </style>
--- src/component/request/Summary.vue
+++ src/component/request/Summary.vue
@@ -2,30 +2,28 @@
     <section class="summary">
         <h3 class="summary__title">Employee's Summary</h3>
         <div class="summary__container">
-            <div class="summary__status status">
-                <h5 class="status__title">Annual leave right</h5>
-                <div class="status__progress">
-                    <div class="status__progress-bar status__progress-bar--blue" style="width: 50%">
+            <div class="summary__status">
+                <h5 class="summary__sub-title">Annual leave right</h5>
+                <div class="summary__progress">
+                    <div class="summary__progress-bar summary__progress-bar--blue" style="width: 50%">
                        50%
                     </div>
                 </div>
             </div>
             <div class="summary__status">
-                <h5 class="status__title">Substitution leave right</h5>
-                <div class="status__progress">
-                    <div class="status__progress-bar status__progress-bar--blue" style="width: 50%">
+                <h5 class="summary__sub-title">Substitution leave right</h5>
+                <div class="summary__progress">
+                    <div class="summary__progress-bar summary__progress-bar--blue" style="width: 50%">
                         50%
                     </div>
-
                 </div>
             </div>
             <div class="summary__status">
-                <h5 class="status__title">Number of Late</h5>
-                <div class="status__progress">
-                    <div class="status__progress-bar status__progress-bar--red" style="width: 50%">
+                <h5 class="summary__sub-title">Number of Late</h5>
+                <div class="summary__progress">
+                    <div class="summary__progress-bar summary__progress-bar--red" style="width: 50%">
                         50%
                     </div>
-
                 </div>
             </div>
         </div>
@@ -52,16 +50,13 @@ export default {
 
         &__title {
             color: rgb(51, 51, 51);
-            font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
             font-size: 20px;
             font-weight: 500;
             line-height: 26.4px;
             margin-bottom: 10px;
         }
-    }
 
-    .status {
-        &__title {
+        &__sub-title {
             margin: 0 0 5px 0;
         }
 
@@ -86,41 +81,5 @@ export default {
                 background-color: #ff6f6d;
             }
         }
-    }
-
-.progress {
-    height: 20px;
-    /*margin-bottom: 20px;*/
-    background-color: #ececec;
-    border-radius: 4px;
-    overflow: hidden;
-}
-    .progress--bar{
-        float: left;
-        width: 0;
-        height: 100%;
-        font-size: 12px;
-        line-height: 20px;
-        color: #fff;
-        text-align: center;
-        box-shadow: inset 0 -1px 0 rgba(0, 0, 0, .15);
-
-        transition: width .5s ease;
-        p{
-            margin: 0;
-            padding:0;
-        }
-    }
-
-    .progress--bar-blue{
-        background-color: #3b96dc;
-    }
-    .progress--bar-red{
-        background-color: #ff6f6d;
-    }
-    .col--md-4{
-        width:32%;
-        float: left;
-        margin: 5px;
     }
 </style>
