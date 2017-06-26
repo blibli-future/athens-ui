e15577b206796cd1eaf3d0d12ba848d0da105be3
aamesaa <azhalia.amesa@si.ukdw.ac.id>
2017-06-26 16:02:20 +0700

    remove style.scss

--- src/component/absensi/Upload.vue
+++ src/component/absensi/Upload.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
+    <div class="upload">
         <alerts></alerts>
         <br/>
-        <h2>Upload Absensi</h2>
+        <h2 class="upload__title">Upload Absensi</h2>
         <br/>
-        <form class="form">
-            <input class="form__section" type="file" name="uploadAbsensi" id="uploadAbsensi"/> <br/><br/>
-            <button class="button" type="submit">Upload</button>
+        <form class="upload__form">
+            <input class="upload__input" type="file" name="uploadAbsensi" id="uploadAbsensi"/> <br/><br/>
+            <button class="upload__button" type="submit">Upload</button>
         </form>
     </div>
 </template>
@@ -26,5 +26,45 @@
 </script>
 
 <style lang="scss">
-    @import "../../_scss/style";
+    .upload{
+        margin: 3% 15%;
+        &__title{
+            display: block;
+            font-size: 1.5em;
+            font-weight: bold;
+        }
+        &__form{
+            margin-bottom: 10px;
+            width:100%;
+        }
+        &__input{
+            border-radius: 5px;
+            height: 2em;
+            border: 1px solid #ccc;
+            padding: 7px 15px;
+            font-family:Arial;
+            font-size:inherit;
+            &[type=file]{
+                border: none;
+            }
+        }
+        &__button{
+            display: inline-block;
+            padding: 8px 12px;
+            margin-bottom: 0;
+            font-size: 14px;
+            text-align: center;
+            border: 1px solid transparent;
+            border-radius: 4px;
+            background-color: #b3dcff;
+            cursor: pointer;
+            &:hover{
+                background-color: #92ccff;
+            }
+        }
+    }
+
+
+
+
 </style>
