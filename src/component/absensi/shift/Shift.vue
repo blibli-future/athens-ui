b7ab3a57576bbde63cd2859a29aed905c9f7e258
aamesaa <azhalia.amesa@si.ukdw.ac.id>
2017-06-27 15:01:10 +0700

    Fix Shift and newShift page scss
    Rename style class and add object on Shift page

--- src/component/absensi/shift/Shift.vue
+++ src/component/absensi/shift/Shift.vue
@@ -1,14 +1,12 @@
 <template>
-    <div class="container">
-
-        <br/>
-        <h2>Shift</h2>
+    <main class="shift">
+        <h2 class="shift__title">Shift</h2>
         <alerts></alerts>
         <br/>
-        <button class="button" type="submit"><router-link to="/shift/add">Add New</router-link> </button>
+        <router-link to="/shift/add" class="shift__button">Add New</router-link>
         <br/><br/>
-        <table>
-            <thead>
+        <table class="table table-hover">
+            <thead class="table__header">
                 <tr>
                     <td>ID Shift</td>
                     <td>Name</td>
@@ -19,20 +17,19 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>{{}}</td>
-                    <td>{{}}</td>
-                    <td>{{}}</td>
-                    <td>{{}}</td>
-                    <td>{{}}</td>
-                    <td>{{}}</td>
-
+                <tr v-for="item in shiftItems"
+                    :key="item.id"
+                    :item="item">
+                    <td>{{item.id}}</td>
+                    <td>{{item.name}}</td>
+                    <td class="text-center">{{item.startHour}}</td>
+                    <td class="text-center">{{item.endHour}}</td>
+                    <td>{{item.department}}</td>
+                    <td>{{item.location}}</td>
                 </tr>
             </tbody>
         </table>
-
-
-    </div>
+    </main>
 </template>
 
 
@@ -40,53 +37,73 @@
     import Alerts from '../../Alert.vue'
 
     export default {
+
         data() {
             return {
+                shiftItems: [
+                    { id: '001', name: 'Cawang Ops' , startHour:"08:00", endHour:"14:00",department:"Operation",location:"Cawang"},
+                    { id: '002', name: 'Cawang Ops' , startHour:"08:00", endHour:"16:00",department:"Operation",location:"Cawang"},
+                    { id: '003', name: 'Batu Ceper' , startHour:"07:00", endHour:"16:00",department:"Operation",location:"Batu Ceper"},
+                    { id: '004', name: 'KS Tubun BES' , startHour:"08:00", endHour:"17:00",department:"Operation",location:"location"},
+                ],
 
             };
         },
 
         components: {Alerts}
-
-
     }
 </script>
 
 <style lang="scss">
-    .container{
-        margin:0 15%;
+
+    .shift{
+        margin:3% 15%;
+        &__title{
+            display: block;
+            font-size: 1.5em;
+            font-weight: bold;
+        }
+        &__button{
+            display: inline-block;
+            padding: 8px 12px;
+            margin-bottom: 0;
+            font-size: 14px;
+            text-align: center;
+            border: 1px solid transparent;
+            border-radius: 4px;
+            background-color: #b3dcff;
+            color: black;
+            cursor: pointer;
+            text-decoration: none;
+            &:hover{
+                background-color: #92ccff;
+            }
+        }
     }
-    table{
+    .table{
         width: 100%;
-        border-collapse: collapse;
-        thead{
+        &__header{
             text-align: center;
             font-weight: bold;
+        }
+        &-hover{
+            tbody{
+                tr:hover{
+                    background-color: #e1e1e1
+                }
+            }
+
         }
         tr{
             td{
                 padding:5px ;
                 border: solid thin #a1a1a1;
-
             }
         }
     }
-    a{
-        text-decoration: none;
+    .text-center{
+        text-align: center
     }
-    .button{
-        display: inline-block;
-        padding: 8px 12px;
-        margin-bottom: 0;
-        font-size: 14px;
-        text-align: center;
-        border: 1px solid transparent;
-        border-radius: 4px;
-        background-color: #b3dcff;
-        cursor: pointer;
-        &:hover{
-            background-color: #92ccff;
-        }
-    }
+
 
 </style>
