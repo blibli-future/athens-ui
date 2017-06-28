b8774270202271460e56e76d8d5291d5ccaafae7
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-06-28 20:55:03 +0700

    Separating panel from history
    separate panel into a new class and apply css for both component

--- src/component/request/History.vue
+++ src/component/request/History.vue
@@ -1,25 +1,16 @@
 <template>
-    <div class="history__item">
-        <h3 class="history__title">Leave History</h3>
-        <div class="history__container">
-            <div class="history__header">
-                <h4 class="history__header--title">Cuti Tahunan</h4>
-                <div class="history__header--date">16 Maret 2017 - 18 Maret 2017</div>
-            </div>
-            <div class="history__body">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            </div>
-        </div>
-    </div>
+    <section class="history">
+        <div class="history__title">Leave History</div>
+        <panel></panel>
+    </section>
 </template>
 
 <script>
+import panel from './history/Panel.vue';
+
 export default {
-  data() {
-    return {};
-  },
-  name: 'request--history'
+    components: { panel },
+    name: 'request--history'
 };
 </script>
 
@@ -41,49 +32,6 @@ export default {
             -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
             box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
             height: 170px;
-        }
-
-        &__header {
-            padding: 1px 18px;
-            border-radius: 5px 5px 0 0;
-            background-color: #c1e5ff;
-            color: #3d7fca;
-
-            &--title {
-                margin: 5px;
-            }
-
-            &--date {
-                margin: 5px;
-            }
-        }
-
-        &__body {
-            padding: 15px;
-            overflow: scroll;
-            height: 100%;
-        }
-    }
-
-    .panels {
-        margin-bottom: 20px;
-        background-color: #fff;
-        border: 1px solid #abd4f8;
-        border-radius: 4px;
-        -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-        box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-        height: 170px;
-
-        &--head{
-            padding: 1px 18px;
-            border-radius: 5px 5px 0 0;
-            background-color: #c1e5ff;
-            color: #3d7fca;
-        }
-        &--body{
-            padding: 15px;
-            overflow: scroll;
-            height: 70px;
         }
     }
 </style>
