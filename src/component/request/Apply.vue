c69b19e2343d2a544391a762efae3ece8d1ffc86
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-06-29 03:13:16 +0700

    Rename apply component's class
    rename classname to (a hopefully) more meaningful name

--- src/component/request/Apply.vue
+++ src/component/request/Apply.vue
@@ -2,19 +2,19 @@
     <section class="apply">
         <h3 class="apply__title">Apply for absence request</h3>
         <form class="apply__wrapper">
-            <div class="apply__wrapper--criteria">
-                <div class="apply__wrapper--main-criteria">
+            <div class="apply__sub-wrapper apply__sub-wrapper--left">
+                <div class="apply__input-wrapper">
                     <label class="apply__label" for="criteria">Criteria</label>
-                    <select class="apply__selector" id="criteria">
+                    <select class="apply__input apply__input--selector" id="criteria">
                         <option>Cuti istimewa</option>
                         <option>Cuti tahunan</option>
                         <option>Ijin sakit</option>
                         <option>Sakit dengan surat dokter</option>
                     </select>
                 </div>
-                <div class="apply__wrapper--sub-criteria">
+                <div class="apply__input-wrapper">
                     <label class="apply__label" for="sub-criteria">Sub Criteria</label>
-                    <select class="apply__selector" id="sub-criteria">
+                    <select class="apply__input apply__input--selector" id="sub-criteria">
                         <option>Cuti istimewa</option>
                         <option>Cuti tahunan</option>
                         <option>Ijin sakit</option>
@@ -22,21 +22,24 @@
                     </select>
                 </div>
             </div>
-            <div class="apply__wrapper--date">
-                <div class="apply__wrapper--start-date">
+            <div class="apply__sub-wrapper apply__sub-wrapper--right">
+                <div class="apply__input-wrapper">
                     <label class="apply__label" for="start-date">Start Date</label>
-                    <input id="start-date" type="date"/>
+                    <input class="apply__input" id="start-date" type="date"/>
                 </div>
-                <div class="apply__wrapper--end-date">
+                <div class="apply__input-wrapper">
                     <label class="apply__label" for="end-date">End Date</label>
-                    <input id="end-date" type="date"/>
+                    <input class="apply__input" id="end-date" type="date"/>
                 </div>
             </div>
-            <div class="apply__wrapper apply__wrapper--reason">
+            <div class="apply__sub-wrapper apply__sub-wrapper--full">
                 <label class="apply__label--reason" for="reason">Reason :</label>
-                <textarea id="reason" class="apply__reason--input"></textarea>
+                <textarea id="reason" class="apply__input--full"></textarea>
             </div>
-            <button class="apply__submit">Apply</button>
+            <div class="apply__input-wrapper">
+                <div class="apply__submit-helper"></div>
+                <button class="apply__submit">Apply</button>
+            </div>
         </form>
     </section>
 </template>
@@ -48,41 +51,69 @@ export default {
 
 <style lang="scss">
     .apply {
+        margin: 1rem 0;
         &__title {
             color: rgb(51, 51, 51);
             font-size: 20px;
             font-weight: 500;
             line-height: 26.4px;
-            margin-bottom: 10px;
         }
 
         &__wrapper {
             display: block;
-            margin: 1rem 0;
+        }
 
-            &--criteria, &--date {
-                display: inline-block;
-                width: 48%;
+        &__sub-wrapper {
+            margin-top: 5px;
+            margin-bottom: 5px;
+            display: inline-block;
+
+            &--left {
+                margin-right: 3%;
+                width: 46%;
             }
 
-            &--main-criteria, &--sub-criteria {
-                margin: 0.5rem 0;
-                display: block;
+            &--right {
+                margin-left: 3%;
+                width: 46%;
             }
+            &--full {
+                width: 100%;
+            }
+        }
 
-            &--start-date, &--end-date {
-                margin: 0.5rem 0;
-                display: block;
-            }
+        &__input-wrapper {
+            margin: 0.5rem 0;
+            display: flex;
         }
 
         &__label {
             width: 100px;
+            padding-top: 5px;
             display: inline-block;
 
             &--reason {
                 display: block;
                 margin: 0 0 5px 0;
+            }
+        }
+
+        &__input {
+            flex-grow: 1;
+            height: 30px;
+
+            &--selector {
+                font-size: 14px;
+                color: #555;
+                background-color: #fff;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+                box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+            }
+
+            &--full {
+                width: 100%;
             }
         }
 
@@ -97,7 +128,7 @@ export default {
             border: 1px solid #ccc;
             border-radius: 4px;
             -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075)
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
         }
 
         &__submit {
@@ -108,10 +139,10 @@ export default {
             border-radius: 4px;
             background-color: #b3dcff;
             cursor: pointer;
-        }
 
-        &__reason--input {
-            width: 100%;
+            &-helper {
+                flex-grow: 1;
+            }
         }
     }
 </style>
