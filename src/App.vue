fe1be84dbfe676831f1fcfabad6b5fc21436e6cb
aamesaa <azhalia.amesa@si.ukdw.ac.id>
2017-06-28 19:44:21 +0700

    remove container on app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <navigation-bar v-if="authenticated"></navigation-bar>
-        <router-view class="container"></router-view>
+        <router-view></router-view>
     </div>
 </template>
 
@@ -20,17 +20,12 @@ export default {
 
 <style lang="scss">
     @import "_scss/default";
-
-    .container{
-        margin:3% 15%;
-    }
   html, body {
       height: 100%;
       width: 100%;
       font-family: Arial;
       margin : 0;
   }
-
   * {
     box-sizing: border-box;
   }
