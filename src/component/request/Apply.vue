847eb33ebe8be3c99ccd192158b14cce2c043ed2
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-07-04 00:30:00 +0700

    create method to post absencePermit
    -no Url yet
    -no empty input checking yet

--- src/component/request/Apply.vue
+++ src/component/request/Apply.vue
@@ -5,40 +5,41 @@
             <div class="apply__sub-wrapper apply__sub-wrapper--left">
                 <div class="apply__input-wrapper">
                     <label class="apply__label" for="criteria">Criteria</label>
-                    <select class="apply__input apply__input--selector" id="criteria">
-                        <option>Cuti istimewa</option>
-                        <option>Cuti tahunan</option>
-                        <option>Ijin sakit</option>
-                        <option>Sakit dengan surat dokter</option>
+                    <select v-model="selectedCriteria"
+                            class="apply__input apply__input--selector" id="criteria">
+                        <option v-for="criterion in Object.keys(criteria)">{{criterion}}</option>
                     </select>
                 </div>
                 <div class="apply__input-wrapper">
                     <label class="apply__label" for="sub-criteria">Sub Criteria</label>
-                    <select class="apply__input apply__input--selector" id="sub-criteria">
-                        <option>Cuti istimewa</option>
-                        <option>Cuti tahunan</option>
-                        <option>Ijin sakit</option>
-                        <option>Sakit dengan surat dokter</option>
+                    <select v-model="selectedSubCriterion"
+                            class="apply__input apply__input--selector" id="sub-criteria">
+                        <option v-for="subCriterion in criteria[selectedCriteria]">{{subCriterion}}</option>
                     </select>
                 </div>
             </div>
             <div class="apply__sub-wrapper apply__sub-wrapper--right">
                 <div class="apply__input-wrapper">
                     <label class="apply__label" for="start-date">Start Date</label>
-                    <input class="apply__input" id="start-date" type="date"/>
+                    <input v-model="startDate"
+                            class="apply__input" id="start-date" type="date"/>
                 </div>
                 <div class="apply__input-wrapper">
                     <label class="apply__label" for="end-date">End Date</label>
-                    <input class="apply__input" id="end-date" type="date"/>
+                    <input v-model="endDate"
+                           class="apply__input" id="end-date" type="date"/>
                 </div>
             </div>
             <div class="apply__sub-wrapper apply__sub-wrapper--full">
                 <label class="apply__label--reason" for="reason">Reason :</label>
-                <textarea id="reason" class="apply__input--full"></textarea>
+                <textarea v-model="reason"
+                        id="reason" class="apply__input--full"></textarea>
             </div>
             <div class="apply__input-wrapper">
                 <div class="apply__submit-helper"></div>
-                <button class="apply__submit">Apply</button>
+                <button v-bind:disabled="selectedCriteria === '---'"
+                        v-on:click="submitRequest"
+                        class="apply__submit">Apply</button>
             </div>
         </form>
     </section>
@@ -46,6 +47,50 @@
 
 <script>
 export default {
+    data() {
+        return {
+            criteria: {
+                '---': ['---'],
+                'absence permit': [
+                    'Hourly',
+                    'Sick',
+                    'Sick with hospital letter',
+                    'Unpaid Leave'
+                ],
+                'leave': []
+            },
+            selectedCriteria: '---',
+            selectedSubCriterion:'',
+            startDate: '',
+            endDate: '',
+            reason: ''
+        }
+    },
+    created: function () {
+//        this.$http.get('UNKNOWN URL WITH NIK AS PATH VARIABLE')
+//            .then((response) => {
+//                //todo: fix code below
+//                this.criteria['leave'] = response.SOMETHING;//Something that will be given from server
+//            })
+//            .catch((errors) => {
+//                console.log(errors)
+//            })
+    },
+    methods: {
+        submitRequest: function () {
+            //todo: fix the url
+            let url = this.selectedCriteria==='absence permit'?
+                'SOMEWHERE FOR ABSENCE PERMIT':
+                'SOMEWHERE FOR LEAVE';
+
+            this.$http.post(url, {
+                'absencePermit': this.selectedSubCriterion.toUpperCase().replace(/ /g, '_'),
+                'startDate': this.startDate,
+                'endDate': this.endDate,
+                'reason': this.reason
+            })//todo: create alert alerting user either its successful or not
+        }
+    }
 }
 </script>
 
