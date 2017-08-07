180ff1288a83de9f08b31bbfaed7f8be4af08370
Dominicus Ariel Christianto <arielchristianto@ti.ukdw.ac.id>
2017-08-07 16:15:36 +0700

    Redesign Unapproved Box to include message

--- src/component/approval/UnapprovedBox.vue
+++ src/component/approval/UnapprovedBox.vue
@@ -1,19 +1,29 @@
 <template>
     <div class="unapproved">
-        <div>
-            <div>
-                <span>{{item.nik}}</span> - <span>{{item.name}}</span>
+        <div class="unapproved__header">
+            <div class="unapproved__text-holder">
+                <div>
+                    <span>{{item.nik}}</span> - <span>{{item.name}}</span>
+                </div>
+                <div>
+                    Type
+                </div>
+                <div>
+                    <span>start date</span> - <span>end date</span>
+                </div>
             </div>
-            <div>
-                Type
-            </div>
-            <div>
-                <span>start date</span> - <span>end date</span>
+            <div class="unapproved__button-holder">
+                <button class="unapproved__button unapproved__button--accept">Accept</button>
+                <button class="unapproved__button unapproved__button--decline">Decline</button>
             </div>
         </div>
-        <div class="unapproved__button-holder">
-            <button class="unapproved__button unapproved__button--accept">Accept</button>
-            <button class="unapproved__button unapproved__button--decline">Decline</button>
+        <div class="unapproved__body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Quisque ornare erat massa, nec laoreet lorem semper ut.
+            Curabitur et arcu lectus. Mauris nec fringilla purus.
+            Duis sit amet ligula ac neque pellentesque mattis.
+            Donec sed malesuada dolor. Vivamus volutpat dapibus dapibus.
+            Duis nec bibendum sapien. Aliquam eu ex sed urna suscipit sodales ut quis lorem.
         </div>
     </div>
 </template>
@@ -31,10 +41,18 @@ export default {
 <style lang="scss">
     .unapproved {
         display: flex;
+        flex-direction: column;
         border-radius: 4px;
         border: 1px solid rgb(188, 232, 241);
-        margin: 10px 0px;
-        padding: 5px;
+        margin: 10px 0;
+
+        &__header {
+            padding: 5px;
+            margin: 0;
+            width: 100%;
+            display: flex;
+            background-color: #c1e5ff;
+        }
 
         &__button-holder {
             margin-left: auto;
@@ -43,8 +61,12 @@ export default {
             flex-direction: column;
         }
 
+        &__body {
+            padding: 5px;
+        }
+
         &__button {
-            margin: 5px 0px;
+            margin: 5px 0;
             border: 1px solid transparent;
             border-radius: 4px;
             cursor: pointer;
